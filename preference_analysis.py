4972291ec856fc1c506ae9ea0935a2d02f46746d
jacob-TUM <jacob_l._miller@hotmail.com>
2024-12-04 18:11:04 +0100

    jacob-experiments

--- preference_analysis.py
+++ preference_analysis.py
@@ -5,63 +5,90 @@ import pylab as plt
 import pandas as pd
 
 
+var = "accuracy"
+data = {
+    "group": [],
+    "value": []
+}
 for n in [10,25,50]:
     n = str(n)
     stress_exp = list()
-    for i,fname in enumerate(["nt_stress50-cleaned.json", "stress50-cleaned.json", "stress_expert-cleaned.json", "preference50-cleaned.json"]):
+    for i,fname in enumerate([f"nt_stress{n}-cleaned.json", f"stress{n}-cleaned.json", f"stress_expert-cleaned.json", f"preference{n}-cleaned.json"]):
         with open(f"json_data/{fname}", 'r') as fdata:
             stress = json.load(fdata)
 
         if 'expert' not in fname:
-            stress_exp.append([s['accuracy'] for s in stress])
-        else: stress_exp.append([s[n]['accuracy'] for s in stress])
+            stress_exp.append([[d for d in s[var] if d >= 0] for s in stress])
+        else: stress_exp.append([[d for d in s[n][var] if d >= 0] for s in stress])
 
 
     stress_exp = {key: s for key,s in zip(['untrained', 'trained', 'expert', 'preference'], stress_exp)}
+
+    stress_exp['gd'] = stress_exp['untrained'] + stress_exp['trained'] + stress_exp['expert']
 
     means = {
         key: np.mean(np.array(tab), axis=1) for key, tab in stress_exp.items()
     }
 
-    data = {
-        "group": sum(([key] * len(val) for key,val in means.items()), start=[]), 
-        "value": sum(([float(x) for x in val] for val in means.values()), start=[])
+    print(len(stress_exp['preference']))
+    print(stress_exp['preference'][0])
+
+    means = {
+        'Experiment 1': [sum(inner) / len(inner) for inner in stress_exp['gd']],
+        'Experiment 2': [sum(inner) / len(inner) for inner in stress_exp['preference']],        
     }
 
-    data = pd.DataFrame(data)
 
-    fig = plt.figure()
+    # data = {
+    #     "group": sum(([key] * len(val) for key,val in means.items()), start=[]), 
+    #     "value": sum(([float(x) for x in val] for val in means.values()), start=[])
+    # }
 
-    ax = sns.violinplot(data=data,x='group',y='value',inner='point')
-    fig.add_axes(ax)
-    ax.set_ylim(0,1)
 
-    from itertools import combinations
-    from scipy.stats import mannwhitneyu
-    for k1,k2 in combinations(means.keys(),2):
-        print(f'{k1} vs. {k2}')
-        test = mannwhitneyu(means[k1], means[k2])
-        print(f'pvalue is {test.pvalue}')
-        print()
+    for key,val in means.items():
+        for x in val:
+            data['group'].append(key)
+            data['value'].append(float(x))
 
-    fig.savefig(f"figures/n{n}_violin.pdf")
-    # plt.show()
 
-    import seaborn as sns
-    import pandas as pd
+gd_experiment = [x for label,x in zip(data['group'], data['value']) if label == 'Experiment 1']
+pr_experiment = [x for label,x in zip(data['group'], data['value']) if label == 'Experiment 2']
 
-    plt.clf()
-    fig = plt.figure()
+data = pd.DataFrame(data)
+# fig = plt.figure()
 
-    # Interval plot using Seaborn's pointplot
-    ax = sns.pointplot(data=data, y="group", x="value", ci=95, capsize=0.2,join=False)
-    sns.stripplot(data=data,x='value', y='group',alpha=0.2)
-    # sns.violinplot(data=data,y='group',x='value',inner=None,ax=ax)
-    ax.set_title("Confidence intervals for mean \"accuracy\" ")
-    ax.set_xlabel("accuracy")
-    
-    ax.set_xlim(0,1)
+# ax = sns.violinplot(data=data,x='group',y='value',inner='point')
+# fig.add_axes(ax)
+# ax.set_ylim(0,1)
 
-    fig.add_axes(ax)
-    # plt.show()    
-    fig.savefig(f'figures/n{n}_interval.pdf')
+from itertools import combinations
+from scipy.stats import mannwhitneyu,wilcoxon
+test = mannwhitneyu(gd_experiment, pr_experiment)
+# test = wilcoxon(means[k1],means[k2])
+print(f'pvalue is {test.pvalue} which is {'' if test.pvalue < 0.05 * (2/12) else 'not'} significant')
+print()
+# plt.show()
+
+import seaborn as sns
+import pandas as pd
+
+plt.clf()
+fig = plt.figure()
+fig.set_dpi(100)
+
+sns.stripplot(data=data,x='value', y='group',alpha=0.2)
+ax = sns.pointplot(data=data, y="group", x="value", ci=95, capsize=0.3,join=False,color='black')
+# sns.violinplot(data=data,y='group',x='value',inner=None,ax=ax)
+ax.set_title(f"Confidence intervals for mean {var} \noverall")
+ax.set_xlabel(f"{var} (s)")
+ax.set_ylabel("")
+
+ax.set_xlim(0,35)
+
+fig.add_axes(ax)
+fig.set_size_inches(4,2)
+# plt.show()    
+fig.savefig(f'figures/all_combined_interval_{var}.pdf',bbox_inches="tight")
+
+print(data.head())
+print(data.shape)
